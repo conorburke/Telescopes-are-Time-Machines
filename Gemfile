a70fa65573f8622fb5a7e297910f71d017a93e1a
conorburke <cjburke89@gmail.com>
2017-01-29 17:56:16 -0800

    set up and pass basic view rendering tests

--- Gemfile
+++ Gemfile
@@ -54,6 +54,7 @@ group :development, :test do
   gem 'capybara'
   gem 'factory_girl_rails'
   gem 'faker'
+  gem 'rails-controller-testing'
 end
 
 group :development do
--- spec/controllers/info_pages_controller_spec.rb
+++ spec/controllers/info_pages_controller_spec.rb
@@ -2,4 +2,30 @@ require 'rails_helper'
 
 RSpec.describe InfoPagesController, type: :controller do
 
+  describe "GET routes" do 
+    it "should render the about page" do
+      get :about
+      expect(response).to be_successful
+      expect(response).to render_template(:about)
+    end
+
+
+    it "should render the help page" do
+      get :contact
+      expect(response).to be_successful
+      expect(response).to render_template(:contact)
+    end    
+
+    it "should render the help page" do
+      get :help
+      expect(response).to be_successful
+      expect(response).to render_template(:help)
+    end
+
+    it "should render the home page" do
+      get :home
+      expect(response).to be_successful
+      expect(response).to render_template(:home)
+    end 
+  end
 end
